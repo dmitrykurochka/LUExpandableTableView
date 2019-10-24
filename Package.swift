491932337de625fc3429b3ee2a22b0be43511059
Dmitry <v.i.p.dimak@gmail.com>
2019-10-24 17:22:56 +0300

    updated spm

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
       .library(name: "LUExpandableTableView", targets: ["LUExpandableTableView"]),
   ],
   targets: [
-      .target(name: "LUExpandableTableView"),
+    .target(name: "LUExpandableTableView", path: "Sources")
   ]
 )
