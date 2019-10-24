eba777e6b67e461d62c56765fb91f6390982ddf6
Dmitry <v.i.p.dimak@gmail.com>
2019-10-24 17:12:31 +0300

    updated target for spm

--- Package.swift
+++ Package.swift
@@ -2,5 +2,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "LUExpandableTableView"
+  name: "LUExpandableTableView",
+  platforms: [
+    .iOS(.v10)
+  ],
+  products: [
+      .library(name: "LUExpandableTableView", targets: ["LUExpandableTableView"]),
+  ],
+  targets: [
+      .target(name: "LUExpandableTableView", path: "LUExpandableTableView"),
+  ]
 )
